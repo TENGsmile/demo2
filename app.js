62ede7309b131f581787e71c3c73d8c172e99575
unknown <unknown@example.com>
2019-02-21 17:36:17 +0800

    2019/2/21
    修改下拉刷新

--- app.js
+++ app.js
@@ -1,65 +1,48 @@
+//app.js
 App({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  globalData: {
+    appid: 'wxd5dadfcadee6b34a',//appid需自己提供，此处的appid我随机编写
+    secret: '3727b0cafe3aa5b5bc8279a0422497e0',//secret需自己提供，此处的secret我随机编写
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLaunch: function () {
+    var that = this
+    var user = wx.getStorageSync('user') || {};
+    var userInfo = wx.getStorageSync('userInfo') || {};
+    if ((!user.openid || (user.expires_in || Date.now()) < (Date.now() + 600)) && (!userInfo.nickName)) {
+      wx.login({
+        success: function (res) {
+          if (res.code) {
+            wx.getUserInfo({
+              success: function (res) {
+                var objz = {};
+                objz.avatarUrl = res.userInfo.avatarUrl;
+                objz.nickName = res.userInfo.nickName;
+                //console.log(objz);
+                wx.setStorageSync('userInfo', objz);//存储userInfo
+              }
+            });
+            var d = that.globalData;//这里存储了appid、secret、token串  
+            var l = 'https://api.weixin.qq.com/sns/jscode2session?appid=' + d.appid + '&secret=' + d.secret + '&js_code=' + res.code + '&grant_type=authorization_code';
+            wx.request({
+              url: l,
+              data: {},
+              method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT  
+              // header: {}, // 设置请求的 header  
+              success: function (res) {
+                var obj = {};
+                obj.openid = res.data.openid;
+                obj.expires_in = Date.now() + res.data.expires_in;
+                console.log(res.data.session_key);
+                wx.setStorageSync('user', obj);//存储openid  
+              }
+            });
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+          }
+        }
+      });
+    }
   }
-})
+}) 
+
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -11,19 +11,17 @@ Page({
     //轮播图数据
     swiperList: ["../image/lun1.jpg", "../image/lun2.jpg", "../image/lun3.jpg"],
     //视频数据
-    videosList: [{ "cn": "订单信息", "en": "order" }, { "cn": "网点信息", "en": "branch" }, { "cn": "设备信息", "en": "machine" }, { "cn": "会员信息", "en": "member" }, { "cn": "产品信息", "en": "product" }, { "cn": "站点信息", "en": "station" }]
-  },
+    videosList: [{ "cn": "订单信息", "en": "order", "count": "" }, { "cn": "网点信息", "en": "branch", "count": "" }, { "cn": "设备信息", "en": "machine", "count": "" }, { "cn": "会员信息", "en": "member", "count": "" }, { "cn": "产品信息", "en": "product", "count": "" }, { "cn": "站点信息", "en": "station", "count": ""}],
+    count: [],
+    newLists:[]
 
-  activeNav(e){
-    this.setData({
-      currentIndexNav:e.target.dataset.index
-    })
   },
+    
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+   
 
   },
 
@@ -31,7 +29,34 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    let that = this;
+    wx.request({
+      url: "http://192.168.2.169:8081/ccsactcom/weixinShow",
+      success(res) {
+        console.log(res.data)
+        that.setData({
+          count: res.data
+        })
+        var newList=[];
+       
+          for (var index in that.data.videosList) {
+            for (var index in that.data.count) {
+            if (index==index){
+              that.data.videosList[index].count = that.data.count[index]
+            
+            }
+              newList.push(that.data.videosList[index])           
+          }
+        }
+       
+        that.setData({
+          newLists: newList
+        })
+        
+        
+      
+      }
+    })
   },
 
   /**
